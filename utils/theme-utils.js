d18b7c018464f85171cbefc1328c273e52c1ff23
apip <outan544@gmail.com>
2025-12-27 16:18:09 +0700

    Update theme-utils.js

--- utils/theme-utils.js
+++ utils/theme-utils.js
@@ -1,6 +1,8 @@
 const { COLOR_THEMES, FONT_THEMES } = require('../themes');
 
-const THEME = process.env.BLOG_THEME || 'reddie';
+const THEME = process.env.BLOG_THEME || 'default';
+const FONT_HEADINGS = process.env.BLOG_FONT_HEADINGS || 'sans-serif';
+const FONT_BODY = process.env.BLOG_FONT_BODY || 'sans-serif';
 
 export function generateCssVariables() {
   const cssVars = {};
@@ -8,9 +10,9 @@ export function generateCssVariables() {
   for (const [key, value] of Object.entries(themeColors)) {
     cssVars[`--theme-${key}`] = value;
   }
-  cssVars['--theme-headings'] = 'Geist' || 'Courier';
-  cssVars['--theme-body'] = 'Geist' || 'Courier';
-  
+  cssVars['--theme-headings'] = FONT_THEMES[FONT_HEADINGS] || 'sans-serif';
+  cssVars['--theme-body'] = FONT_THEMES[FONT_BODY] || 'sans-serif';
+
   const cssVarsString = Object.entries(cssVars)
     .map(([key, value]) => `${key}: ${value};`)
     .join('\n');
