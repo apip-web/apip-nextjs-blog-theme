5a51c071e65e8011fb7d216987e36fa32ea4c7f3
apip <outan544@gmail.com>
2025-12-27 21:53:56 +0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -15,5 +15,45 @@ export function GradientBackground({ variant, className }) {
 }
 
 export default function Layout({ children }) {
-  return <>{children}</>;
+  const setAppTheme = () => {
+    const darkMode = localStorage.getItem('theme') === 'dark';
+    const lightMode = localStorage.getItem('theme') === 'light';
+
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else if (lightMode) {
+      document.documentElement.classList.remove('dark');
+    }
+    return;
+  };
+
+  const handleSystemThemeChange = () => {
+    var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    darkQuery.onchange = (e) => {
+      if (e.matches) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+      }
+    };
+  };
+
+  useEffect(() => {
+    setAppTheme();
+  }, []);
+
+  useEffect(() => {
+    handleSystemThemeChange();
+  }, []);
+
+  return (
+    <div className="relative pb-24 overflow-hidden">
+      <div className="flex flex-col items-center w-full max-w-2xl mx-auto">
+        {children}
+      </div>
+    </div>
+  );
 }
