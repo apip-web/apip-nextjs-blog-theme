ef854c3cd6e34ec80591fd5aaf74d30b31ab7ee0
apip <outan544@gmail.com>
2025-12-27 16:39:33 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import { Geist } from 'next/font/google';
+
+// Definisi Geist
+const geist = Geist({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-geist',
+  display: 'swap',
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <span className="theme-bejamas" />
-      <Component {...pageProps} />
+      <main className={`${geist.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
