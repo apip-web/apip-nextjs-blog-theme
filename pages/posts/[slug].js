d19a984f1fe4ba10fc517ccefd02bf4771897769
apip <outan544@gmail.com>
2025-12-28 07:56:55 +0700

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -46,7 +46,7 @@ export default function PostPage({
         description={frontMatter.description}
       />
       <Header name={globalData.name} />
-      <article className="px-6 md:px-0" data-sb-object-id={`posts/${slug}.mdx`}>
+      <article className="prose px-6 md:px-0" data-sb-object-id={`posts/${slug}.mdx`}>
         <header>
           <h1
             className="mb-12 text-3xl text-center md:text-5xl dark:text-white"
