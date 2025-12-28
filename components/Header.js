b0608ba02ab07e0cd35f0503a8efae07c8228c7e
apip <outan544@gmail.com>
2025-12-28 10:01:59 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,33 @@
 import Link from 'next/link';
+import Image from 'next/image'; // pastikan pakai Next.js Image
 
-export default function Header({ name }) {
+export default function Header({ name, authorImg }) {
+  // split name untuk highlight "Web"
+  const [firstName, lastName] = name.split(' ');
+
   return (
-    <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
-      <p className="header-author text-2xl text-center dark:text-white">
-        <Link href="/">{name}</Link>
-      <span className="text-red-500"> Web</span>
+    <header className="pt-20 pb-12 flex flex-col items-center">
+      {/* lingkaran warna seolah border */}
+      <div className="relative w-16 h-16 mb-4">
+        <div className="absolute inset-0 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100% blur-[4px]" />
+        {authorImg && (
+          <Image
+            src={authorImg}
+            alt={name}
+            width={64}
+            height={64}
+            className="relative rounded-full border-4 border-white dark:border-black"
+          />
+        )}
+      </div>
+
+      {/* Nama author */}
+      <p className="header-author text-2xl text-center dark:text-white font-semibold">
+        <Link href="/">
+          {firstName}{' '}
+          <span className="text-red-500">{lastName}</span>
+        </Link>
       </p>
     </header>
   );
-}
+        }
