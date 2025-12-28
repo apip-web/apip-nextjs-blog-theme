9c7bbc83a7b505199d7e18c5f258743689c644cc
apip <outan544@gmail.com>
2025-12-28 08:28:14 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,9 @@ export default function Header({ name }) {
   return (
     <header className="prose pt-20 pb-12">
       <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
-      <p className="text-2xl text-center dark:text-white">
+      <h1 className="text-2xl text-center dark:text-white">
         <Link href="/">{name}</Link>
-      </p>
+      </h1>
     </header>
   );
 }
